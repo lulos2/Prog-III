41ac33ac36c25bbe004a7211587f8fee4ad2cbae
Lulo <lulobareiss@gmail.com>
2023-06-11 17:12:58 -0300

    tester refactor

--- Main.java
+++ Main.java
@@ -1,22 +1,29 @@
 package practicoEspecialP2;
 
 import practicoEspecialP2.services.ShortNetBacktracking;
-
-import java.io.Reader;
+import practicoEspecialP2.services.ShortNetGreedy;
+import practicoEspecialP2.services.ShortNetKruskal;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\lulo\\Documents\\tudai\\2do\\PROG-III\\prog3\\src\\practicoEspecialP2\\datasets\\dataset1.txt";
+        String path = "C:\\Users\\lulo\\Documents\\tudai\\2do\\PROG-III\\prog3\\src\\practicoEspecialP2\\datasets\\dataset2.txt";
         CSVReader reader = new CSVReader(path);
         reader.read();
 
-
+        ShortNetGreedy greedy = new ShortNetGreedy(reader);
+        ShortNetKruskal Kruskal = new ShortNetKruskal(reader);
         ShortNetBacktracking backtracking = new ShortNetBacktracking(reader);
-        System.out.println(backtracking.findMinimumCoverTree());
-        System.out.println(backtracking.getMinimunDistanceForConectEveryStations());
-        System.out.println(backtracking.getIterations());
+        /*System.out.println(backtracking.findMinimumCoverTree());
+        System.out.println(backtracking.getMinimumDistanceForConnectEveryStations());
+        System.out.println(backtracking.getIterations());*/
+        System.out.println(Kruskal.findMinimumCoverTree());
+        System.out.println(Kruskal.getMinimumDistanceForConnectEveryStations());
+        System.out.println(Kruskal.getIterations());
+        System.out.println(greedy.findMinimumCoverTree());
+        System.out.println(greedy.getMinimumDistanceForConnectEveryStations());
+        System.out.println(greedy.getIterations());
     }
 
 }
